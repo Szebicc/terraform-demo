4b46574923e5fd9f69821a4f4eddbc89c09b5a61
Szebicc <163783319+Szebicc@users.noreply.github.com>
2025-06-05 17:16:08 +0200

    removed unnecessary addons and included external ingress

--- terraform/modules/eks/addons.tf
+++ terraform/modules/eks/addons.tf
@@ -13,13 +13,13 @@ resource "aws_eks_addon" "kube-proxy" {
   addon_name    = "kube-proxy"
 }
 
-resource "aws_eks_addon" "ebs_csi" {
-    cluster_name              = aws_eks_cluster.this.name
-    addon_name                = "aws-ebs-csi-driver"
-    service_account_role_arn  = aws_iam_role.ebs_csi.arn 
-}
+# resource "aws_eks_addon" "ebs_csi" {
+#     cluster_name              = aws_eks_cluster.this.name
+#     addon_name                = "aws-ebs-csi-driver"
+#     service_account_role_arn  = aws_iam_role.ebs_csi.arn 
+# }
 
-resource "aws_eks_addon" "cloudwatch_observability" {
-    cluster_name  = aws_eks_cluster.this.name
-    addon_name    = "amazon-cloudwatch_observability"
-}
+# resource "aws_eks_addon" "cloudwatch_observability" {
+#     cluster_name  = aws_eks_cluster.this.name
+#     addon_name    = "amazon-cloudwatch_observability"
+# }
